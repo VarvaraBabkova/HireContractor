52154b02227f787390bf5709ba81f402e9582269
Varvara Babkova <varvara.munday@gmail.com>
2019-08-19 11:37:32 -0500

    contractor menu

--- hc_frontend/index.js
+++ hc_frontend/index.js
@@ -1,3 +1,13 @@
 fetch("http://localhost:3000/api/v1/contractors")
 .then(res => res.json())
-.then(r => console.log(r))
+.then(contractors => {
+	
+	let sidenav = document.querySelector(".sidenav")
+	sidenav.innerHTML = ""
+	contractors.forEach(c => {
+		let ahref = document.createElement("a")
+		ahref.innerText = c.name
+		sidenav.append(ahref)
+	})
+	console.log(contractors)
+})
