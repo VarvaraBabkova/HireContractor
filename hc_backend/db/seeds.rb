108336ff507db3dcc9ebf73356342717b6d35cfe
Varvara Babkova <varvara.munday@gmail.com>
2019-08-20 14:23:56 -0500

    peeking contractors

--- hc_backend/db/seeds.rb
+++ hc_backend/db/seeds.rb
@@ -10,36 +10,61 @@ Contractor.destroy_all
 Client.destroy_all
 Appointment.destroy_all
 
-cat1 = Category.create(name: "Plumbing")
-cat2 = Category.create(name: "Housekeeping")
+cat1 = Category.create(name: "Plumbers")
+cat2 = Category.create(name: "Housekeepers")
+cat3 = Category.create(name: "Gardeners")
 
 
-c1 = Contractor.create(name: "Joe Shmoe", price: 20, work: "Chris gets it Done.", 
-	img:"https://ehmsolutions.org/wp-content/uploads/2018/04/construction-worker-200x374.png", 
-	intro:" I believe that every client should be 100% satisfied with the work I complete for them. 
-	If this is not the case I am always willing to go out and make changes to meet their expectations. 
-		I treat every project like it was being done in my own home.", category_id: cat1.id )
-c2 = Contractor.create(name: "Mary Leal", price: 10, work: "Houston's cleaning, packing,unpacking", 
-	img:"https://image.shutterstock.com/image-photo/attractive-beautiful-asian-woman-maid-260nw-704724562.jpg",
-intro: "$31 per hr. + tax. $30 off your 1st visit! You're in control of the time and duties.
- 3 hour minimum. Over 540 excellent, verified reviews online available on request. 
- Insured. Bonded. 30 years in business! Text or call me! Ginger, Abso Clean, Inc.", category_id: cat2.id )
 
-
-c3 = Contractor.create(name: "Joe Shmoe", price: 20, work: "Chris gets it Done.", 
+c1 = Contractor.create(name: "Harry", price: 30, work: "Chris gets it Done.", 
 	img:"https://ehmsolutions.org/wp-content/uploads/2018/04/construction-worker-200x374.png", 
 	intro:" I believe that every client should be 100% satisfied with the work I complete for them. 
 	If this is not the case I am always willing to go out and make changes to meet their expectations. 
 		I treat every project like it was being done in my own home.", category_id: cat1.id )
 
+c3 = Contractor.create(name: "Barry", price: 30, work: "Chris gets it Done.", 
+	img:"http://streatcrete.co.uk/wp-content/uploads/2015/12/worker.png", 
+	intro:" I hanges to ", category_id: cat1.id )
 
+c4 = Contractor.create(name: "Larry", price: 30, work: "Chris gets it Done.", 
+	img:"http://freepngdownload.com/image/thumb/industrial-worker-png-free-download-26.png", 
+	intro:" I hanges to ", category_id: cat1.id )
 
-cl1 = Client.create(name: "Ryan Bell")
-cl2 = Client.create(name: "Chris Ryan")
+c5 = Contractor.create(name: "Varry", price: 30, work: "Chris gets it Done.", 
+	img:"https://www.euspray.com/wp-content/uploads/2017/09/handyman.png", 
+	intro:" I hanges to ", category_id: cat1.id )
+c6 = Contractor.create(name: "Varry", price: 30, work: "Chris gets it Done.", 
+	img:"https://i.pinimg.com/236x/3c/b8/d7/3cb8d77747c1c7e4255a488e70560dad--handyman-service-dad-daughter.jpg", 
+	intro:" I hanges to ", category_id: cat1.id )
+c7 = Contractor.create(name: "Varry", price: 30, work: "Chris gets it Done.", 
+	img:"https://www.serviceandsolution.ae/wp-content/uploads/2017/12/contractor.png", 
+	intro:" I hanges to ", category_id: cat1.id )
 
-a1 = Appointment.create(client_id: cl1.id,contractor_id: c1.id)
-a2 = Appointment.create(client_id: cl1.id,contractor_id: c2.id)
-a3 = Appointment.create(client_id: cl2.id,contractor_id: c1.id)
+h1 = Contractor.create(name: "Mary", price: 20, work: "Houston's cleaning, packing,unpacking", 
+	img:"https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/e475f86d-5568-441a-99ce-4f2af15c3623/File/f4386af2ca1ee6ac9e96f05e33cd5221/shutterstock_654521407.jpg",
+	intro: "", category_id: cat2.id )
+h2 = Contractor.create(name: "Ann", price: 20, work: "Houston's cleaning, packing,unpacking", 
+	img:"https://i.pinimg.com/originals/5e/e3/30/5ee330ae4ea47113f26efcfab61c1bca.jpg",
+	intro: "", category_id: cat2.id )
+h3 = Contractor.create(name: "Clair", price: 20, work: "Houston's cleaning, packing,unpacking", 
+	img:"https://img-aws.ehowcdn.com/340x221p/photos.demandstudios.com/getty/article/181/165/161146656.jpg",
+	intro: "", category_id: cat2.id )
+h4 = Contractor.create(name: "Dorris", price: 20, work: "Houston's cleaning, packing,unpacking", 
+	img:"http://daewonthuduc.com/uploadwb/hinhanh/12d62b8_1_79872018125213_b_.jpg",
+	intro: "", category_id: cat2.id )
+
+g1 = Contractor.create(name: "Bob", price: 10, work: "Houston's gardening", 
+	img:"https://www.allianz.com.au/business/business-insurance/news/small-business-insurance-for-gardeners-and-landscapers/local-image/small-business-insurance-for-gardeners-and-landscapers-01_360x240.jpg",
+	intro: "", category_id: cat3.id )
+g2 = Contractor.create(name: "Joe", price: 10, work: "Houston's gardenin", 
+	img:"https://q7i2y6d5.stackpathcdn.com/wp-content/uploads/2014/11/gardener1-400x266.jpg",
+	intro: "", category_id: cat3.id )
+g3 = Contractor.create(name: "Bob", price: 10, work: "Houston's gardenin", 
+	img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbHdWK9mNA-j_a7p9nsl9SMuGwtzretZpcxFwdRxvM-ACS39qx8A",
+	intro: "", category_id: cat3.id )
+# a1 = Appointment.create(client_id: cl1.id,contractor_id: c1.id)
+# a2 = Appointment.create(client_id: cl1.id,contractor_id: c2.id)
+# a3 = Appointment.create(client_id: cl2.id,contractor_id: c1.id)
 
 
 # t.string :name
--- hc_frontend/index.js
+++ hc_frontend/index.js
@@ -1,91 +1,138 @@
 contractorURL = "http://localhost:3000/api/v1/contractors"
 clientURL ="http://localhost:3000/api/v1/clients"
 apptURL = "http://localhost:3000/api/v1/appointments"
-
-
-function renderContractor(){
-    const div = document.createElement("div")
-
-    const h2 = document.createElement("h2")
-    h2.innerText 
-
-    const p = document.createElement("p")
-    p.innerText = contractors.appointments.date 
-
-    div.append(bttn)
-    divCreate.append(div)
-}
-
-
-function newAppointment(name){
-    fetch(contractorURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "app[lication/json"
-        },
-        body: JSON.stringify({
-            name: client.id,
-            date: date,
-            time: time
-        })
-    })
-    .then(res => res.json())
-    .then(task => renderTask(task))
-}
+const init_fund = 100
 
 const main = document.querySelector(".main")
+let chosen = []
+let my_menu = document.querySelector(".my_menu")
+
 document.addEventListener("DOMContentLoaded", () => {
+	let my_menu = document.querySelector(".my_menu")
+
+	let fund = document.createElement("h2")
+	fund.innerText = init_fund
+	fund.className = "fund"
+	my_menu.append(fund)
 
 	fetch("http://localhost:3000/api/v1/categories")
 	.then(res => res.json())
 	.then(categories => {
-		let sidenav = document.querySelector(".sidenav")
+		let barmenu = document.querySelector(".navbar")
 
-		sidenav.innerHTML = ""
+		barmenu.innerHTML = ""
 		
 		categories.forEach(c => {
 			let ahref = document.createElement("button")
 			ahref.innerText = c.name
-			sidenav.append(ahref)
-
+			barmenu.append(ahref)
 			ahref.addEventListener("click", () =>{
-				fill_contractors_menu(c.contractors)
+				fill_contractors_list(c.contractors)
 			})
 		})
+
+		fill_contractors_list(categories[0].contractors)
 		console.log(categories)
 	})
 
 
-	const formCreate = document.querySelector(".initialize-appt")
+	// const formCreate = document.querySelector(".initialize-appt")
 
-	formCreate.addEventListener("submit", e => {
-	    e.preventDefault()
-	})
+	// formCreate.addEventListener("submit", e => {
+	//     e.preventDefault()
+	// })
 	
 }) //DOM
 
 
+function fill_contractors_list(contractors) {
+	let main = document.querySelector(".main")
+	let my_menu = document.querySelector(".my_menu")
 
-
-
-function fill_contractors_menu(contractors) {
-	let sidenav = document.querySelector(".sidenav")
-
-	sidenav.innerHTML = ""
-	//debugger
-	render_card(contractors[0])
+	main.setAttribute("category", contractors[0].category_id)
+	main.innerHTML = ""
+	//my_menu.innerHTML = ""
 
 	contractors.forEach(c => {
-		let ahref = document.createElement("button")
-		ahref.innerText = c.name
-		sidenav.append(ahref)
+		let div_polar = render_mini_card(c)
+		if (chosen.includes(c.id)){
+			
+		 } else {
+			main.append(div_polar)
+		 }
 
-		ahref.addEventListener("click", () =>{
-			render_card(c)
+		div_polar.addEventListener("click", () =>{
+			if (div_polar.style.opacity < 1) 
+				return
+
+			console.log(c)
+			console.log(chosen.includes(c.id))
+
+			if (chosen.includes(c.id)){
+				chosen = chosen.filter(id => id != c.id)
+				my_menu.querySelector(".fund").innerText  = parseInt(my_menu.querySelector(".fund").innerText) + c.price
+
+				if (main.getAttribute("category") == c.category_id)
+				{
+					main.append(div_polar)
+				}else{
+					my_menu.removeChild(div_polar)
+				}
+
+				able_disable(main, parseInt(my_menu.querySelector(".fund").innerText))
+
+				console.log(my_menu)
+			 } else {
+				my_menu.append(div_polar)
+				chosen.push(c.id)
+				my_menu.querySelector(".fund").innerText = parseInt(my_menu.querySelector(".fund").innerText) - c.price
+				able_disable(main, parseInt(my_menu.querySelector(".fund").innerText))
+
+			 }
+			
+			console.log(chosen)
 		})
+		able_disable(main, parseInt(my_menu.querySelector(".fund").innerText))
 	})
-	console.log(contractors)
 	
+}
+
+function able_disable(list, fund) {
+	list.childNodes.forEach(c => {
+		//console.log(c.getAttribute("contractor_price"))
+		if (c.getAttribute("contractor_price") <= fund) {
+			c.style.opacity = 1
+		}else
+			c.style.opacity = 0.3
+
+	})
+}
+
+function render_mini_card(c) {
+
+	let h4 = document.createElement("h4")
+	h4.innerText = "Price " + c.price + "$"
+
+	let h4_name = document.createElement("h4")
+	h4_name.innerText = c.name
+
+	
+	let div_polar = document.createElement("div")
+	div_polar.className = "gallery"
+
+	let img = document.createElement("img")
+	img.src = c.img
+
+	let div_cont = document.createElement("div")
+	div_cont.className = "desc"
+
+	div_cont.append(h4_name, h4)
+	div_polar.append(img, div_cont)
+
+	div_polar.setAttribute("contractor_id", c.id)
+	div_polar.setAttribute("contractor_price", c.price)
+
+	return div_polar
 }
 
 // fetch("http://localhost:3000/api/v1/contractors")
@@ -110,6 +157,35 @@ function fill_contractors_menu(contractors) {
 // 	console.log(contractors)
 // })
 
+// function renderContractor(){
+//     const div = document.createElement("div")
+
+//     const h2 = document.createElement("h2")
+//     h2.innerText 
+
+//     const p = document.createElement("p")
+//     p.innerText = contractors.appointments.date 
+
+//     div.append(bttn)
+//     divCreate.append(div)
+// }
+
+
+// function newAppointment(name){
+//     fetch(contractorURL, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "app[lication/json"
+//         },
+//         body: JSON.stringify({
+//             name: client.id,
+//             date: date,
+//             time: time
+//         })
+//     })
+//     .then(res => res.json())
+//     .then(task => renderTask(task))
+// }
 
 
 function render_card(c) {
