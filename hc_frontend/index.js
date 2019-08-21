fc7e3eac2c57361c1bc58eca6d80f78d7cb1a844
Varvara Babkova <varvara.munday@gmail.com>
2019-08-21 14:24:48 -0500

    line with no's

--- hc_frontend/index.js
+++ hc_frontend/index.js
@@ -1,6 +1,9 @@
 contractorURL = "http://localhost:3000/api/v1/contractors"
 clientURL ="http://localhost:3000/api/v1/clients"
 apptURL = "http://localhost:3000/api/v1/appointments"
+
+price_tag_image = "price-tag.png"
+
 const init_fund = 100
 
 const main = document.querySelector(".main")
@@ -68,82 +71,32 @@ document.addEventListener("DOMContentLoaded", () => {
 	
 }) //DOM
 
+
+
 function fill_contractors_line(contractors) {
 	let main = document.querySelector(".main")
+
 	let my_menu = document.querySelector(".my_menu")
 
 	main.innerHTML = ""
+	chosen = []
 
 	contractors.forEach(c => {
-		let div_polar = render_mini_card(c, "red-question-mark.png")//"qmark.jpg"
+		let div_polar = render_mini_card(c, "red-question-mark.png", price_tag = false)//"qmark.jpg"
 		
 		main.append(div_polar)
 		 
-
 		div_polar.addEventListener("click", () =>{
-			// if (div_polar.style.opacity < 1) 
-			// 	return
-
-			// console.log(c)
-			// console.log(chosen.includes(c.id))
-
-			// if (chosen.includes(c.id)){
-			// 	chosen = chosen.filter(id => id != c.id)
-			// 	my_menu.querySelector(".fund").innerText  = parseInt(my_menu.querySelector(".fund").innerText) + c.price
-
-			// 	if (main.getAttribute("category") == c.category_id)
-			// 	{
-			// 		main.append(div_polar)
-			// 	}else{
-			// 		my_menu.removeChild(div_polar)
-			// 	}
-
-			// 	able_disable(main, parseInt(my_menu.querySelector(".fund").innerText))
-
-			// 	console.log(my_menu)
-			//  } else {
-			// 	my_menu.append(div_polar)
-			// 	chosen.push(c.id)
-			// 	my_menu.querySelector(".fund").innerText = parseInt(my_menu.querySelector(".fund").innerText) - c.price
-			// 	able_disable(main, parseInt(my_menu.querySelector(".fund").innerText))
-
-			//  }
+			if (div_polar.style.opacity < 1) 
+				return
 			
+			disable_all(main)
+			chosen.push(c.id)
+
+			draw_mymenu_choice(my_menu, c, div_polar)			
 		})
-		able_disable(main, 0)
+		able_disable_kicked(main, chosen)
 	})
-
-	let flag = true
-	let yes_btn = document.createElement("button")
-	yes_btn.innerText = "Accept offer!"
-	yes_btn.className = "button green"
-	let no_btn = document.createElement("button")
-	no_btn.innerText = "Decline offer!"
-	no_btn.className = "button red"
-	let quote_text = document.createElement("h2")
-
-	my_menu.append(yes_btn)
-	my_menu.append(no_btn)
-	my_menu.append(quote_text)
-
-
-	yes_btn.addEventListener("click", () => {
-		flag = false
-	})
-
-	no_btn.addEventListener("click", () => {
-		if (contractors.length > 0) {
-			quote_text.innerText = contractors.splice(contractors.length * Math.random() | 0, 1)[0].price
-
-		}
-	})
-	
-	//while ((contractors.length > line_length) && (flag)) {
-		//contractors.splice(contractors.length * Math.random() | 0, 1)[0];
-	quote_text.innerText = contractors.splice(contractors.length * Math.random() | 0, 1)[0].price
-
-	//}
-
 	
 }
 
@@ -195,14 +148,78 @@ function fill_contractors_list(contractors) {
 			
 			console.log(chosen)
 		})
-		able_disable(main, parseInt(my_menu.querySelector(".fund").innerText))
+		able_disable_price(main, parseInt(my_menu.querySelector(".fund").innerText))
 	})
 	
 }
 
-function able_disable(list, fund) {
+function draw_mymenu_choice(my_menu, contractor, div_polar) {
+	let yes_btn = document.createElement("button")
+	yes_btn.innerText = "Accept offer!"
+	yes_btn.className = "button green"
+	let no_btn = document.createElement("button")
+	no_btn.innerText = "Decline offer!"
+	no_btn.className = "button red"
+	let quote_text = document.createElement("h2")
+
+
+	change_to_tag(div_polar, contractor.price)
+	div_polar.style.opacity = 1
+	my_menu.append(div_polar)
+	my_menu.append(yes_btn)
+	my_menu.append(no_btn)
+	my_menu.append(quote_text)
+
+	quote_text.innerText = contractor.price
+
+	yes_btn.addEventListener("click", () => {
+		//able_disable_kicked(document.querySelector(".main"), chosen)
+
+	})
+
+	no_btn.addEventListener("click", () => {
+		document.querySelector(".main").append(div_polar)
+		able_disable_kicked(document.querySelector(".main"), chosen)
+		my_menu.innerHTML = ""
+
+	})
+
+}
+function change_to_tag(div_polar, price) {
+	console.log(div_polar, price)
+
+	div_polar.removeChild(div_polar.children[1])
+
+	let tag_img = document.createElement("img")
+	let h4 = document.createElement("h4")
+
+	tag_img.src = price_tag_image
+	tag_img.style.position = "absolute"
+	tag_img.style.top = "0"
+	tag_img.style.right = "0"
+
+	 tag_img.style.zIndex = "10"
+	 tag_img.style.backgroundColor = "transparent"
+	 tag_img.style.height = "55px"
+	 tag_img.style.width = "55px"
+
+	  h4.innerText = price + "$"
+	h4.style.position = "absolute"
+	h4.style.top = "0"
+	h4.style.right = "0"
+	h4.style.zIndex = "20"
+	 h4.style.backgroundColor = "transparent"
+	 h4.style.height = "50px"
+	 h4.style.width = "40px"
+	
+
+	div_polar.append(tag_img, h4)
+	console.log(div_polar)
+}
+
+
+function able_disable_price(list, fund) {
 	list.childNodes.forEach(c => {
-		//console.log(c.getAttribute("contractor_price"))
 		if (c.getAttribute("contractor_price") <= fund) {
 			c.style.opacity = 1
 		}else
@@ -211,10 +228,22 @@ function able_disable(list, fund) {
 	})
 }
 
-function render_mini_card(c, extra = null) {
+function able_disable_kicked(list, chosen) {
+	list.childNodes.forEach(c => {
 
-	let h4 = document.createElement("h4")
-	h4.innerText = "Price " + c.price + "$"
+		if (!chosen.includes(parseInt(c.getAttribute("contractor_id")))) {
+			c.style.opacity = 1
+
+		}else
+			c.style.opacity = 0.3
+	})
+}
+function disable_all(list) {
+	list.childNodes.forEach(c => c.style.opacity = 0.3)
+}
+
+
+function render_mini_card(c, extra = null, price_tag = true) {
 
 	let h4_name = document.createElement("h4")
 	h4_name.innerText = c.name
@@ -229,6 +258,7 @@ function render_mini_card(c, extra = null) {
 
 	
 	let ex_img = document.createElement("img")
+	ex_img.id = "extra_image"
 
 	if (extra) {
 		ex_img.src = extra
@@ -241,76 +271,39 @@ function render_mini_card(c, extra = null) {
 		 ex_img.style.height = "150px"
 	}
 	
+	let tag_img = document.createElement("img")
+	let h4 = document.createElement("h4")
 
-	let div_cont = document.createElement("div")
-	div_cont.className = "desc"
 
-	//div_cont.append(h4_name, h4)
-	div_polar.append(img, ex_img)
+	if (price_tag) {
+		tag_img.src = price_tag_image
+		tag_img.style.position = "absolute"
+		tag_img.style.top = "0"
+		tag_img.style.right = "0"
+
+		 tag_img.style.zIndex = "10"
+		 tag_img.style.backgroundColor = "transparent"
+		 tag_img.style.height = "55px"
+		 tag_img.style.width = "55px"
+
+		  h4.innerText = c.price + "$"
+		h4.style.position = "absolute"
+		h4.style.top = "0"
+		h4.style.right = "0"
+		h4.style.zIndex = "20"
+		 h4.style.backgroundColor = "transparent"
+		 h4.style.height = "50px"
+		 h4.style.width = "40px"
+	}
+	
+
+	div_polar.append(img, ex_img, tag_img, h4)
 
 	div_polar.setAttribute("contractor_id", c.id)
 	div_polar.setAttribute("contractor_price", c.price)
 
 	return div_polar
 }
-
-function fun_background() {
-	const div1 = document.createElement("div")
-	div1.className = "circle"
-	return div1
-}
-
-// fetch("http://localhost:3000/api/v1/contractors")
-// .then(res => res.json())
-// .then(contractors => {
-	
-// 	let sidenav = document.querySelector(".sidenav")
-
-// 	sidenav.innerHTML = ""
-// 	render_card(contractors[0])
-
-// 	contractors.forEach(c => {
-// 		let ahref = document.createElement("button")
-// 		ahref.innerText = c.name
-// 		sidenav.append(ahref)
-
-
-// 		ahref.addEventListener("click", () =>{
-// 			render_card(c)
-// 		})
-// 	})
-// 	console.log(contractors)
-// })
-
-// function renderContractor(){
-//     const div = document.createElement("div")
-
-//     const h2 = document.createElement("h2")
-//     h2.innerText 
-
-//     const p = document.createElement("p")
-//     p.innerText = contractors.appointments.date 
-
-//     div.append(bttn)
-//     divCreate.append(div)
-// }
-
-
-// function newAppointment(name){
-//     fetch(contractorURL, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "app[lication/json"
-//         },
-//         body: JSON.stringify({
-//             name: client.id,
-//             date: date,
-//             time: time
-//         })
-//     })
-//     .then(res => res.json())
-//     .then(task => renderTask(task))
-// }
 
 
 
